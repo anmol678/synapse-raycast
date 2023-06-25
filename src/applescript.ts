d53f2ac5b401ec2f4eccf5ec87f551efe2e34b54
Anmol Jain <anmolj678@gmail.com>
2023-06-25 15:37:13 -0700

    lint

--- src/applescript.ts
+++ src/applescript.ts
@@ -3,7 +3,7 @@ import { convertSafariStringToJson, parseArrayFromString } from "./utils"
 import { SafariTabProps } from "./types"
 
 export async function performSafariAction(actionScript: string, humanReadableOutput: boolean = true) {
-    let script = `
+    const script = `
         tell application "System Events"
             set activeApp to name of first application process whose frontmost is true
         end tell
@@ -27,18 +27,18 @@ export async function performSafariAction(actionScript: string, humanReadableOut
 // Knowledge Management Extensions
 
 export async function getActiveTabDetails(): Promise<SafariTabProps> {
-    let script = `
+    const script = `
         set theURL to URL of front document
         set theHTML to do JavaScript "document.body.innerText" in front document
         return {url: theURL, html: theHTML}
     `
 
-    let result = await performSafariAction(script)
+    const result = await performSafariAction(script)
     return convertSafariStringToJson(result)
 }
 
 export async function openNewTabAndEnterText(url: string, text: string) {
-    let script = `
+    const script = `
         tell application "Safari"
             set originalTab to current tab of front window
             make new tab at end of tabs of front window with properties {URL: "${url}"}
